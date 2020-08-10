3b63f35965b6fd9598793273cf47aee684fc3878
Kacper Szemraj <kacperprzemyslawszemraj@gmail.com>
2020-08-10 12:20:56 +0200

    tasks content deleted, saving tasks in local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
@@ -8,11 +8,12 @@ import Container from "./Container";
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState([
-    { id: 1, content: "Zadanie 1", done: true },
-    { id: 2, content: "Zadanie 2", done: false },
-    { id: 3, content: "Zadanie 3", done: true },
-  ]);
+  const [tasks, setTasks] =
+    useState(JSON.parse(localStorage.getItem("tasks")) || []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  }, [tasks]);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
