a850b62c1b848e397c4eb1149d901e5aabc6f7a5
Kacper Szemraj <kacperprzemyslawszemraj@gmail.com>
2020-09-08 16:09:20 +0200

    properties changed to the theme properties

--- src/Section/styled.js
+++ src/Section/styled.js
@@ -2,12 +2,12 @@ import styled from "styled-components";
 
 export const Wrapper = styled.section`
     margin: 10px 0;
-    background: #fff;
-    box-shadow: 0 0 5px #ddd;
+    background: ${({ theme }) => theme.color.white};
+    box-shadow: 0 0 5px ${({ theme }) => theme.color.alto};
 `;
 
 export const Header = styled.header`
-    border-bottom: 1px solid #ddd;
+    border-bottom: 1px solid ${({ theme }) => theme.color.alto};
     padding: 20px;
     display: grid;
     grid-template-columns: auto auto;
@@ -15,7 +15,7 @@ export const Header = styled.header`
     justify-content: space-between;
     align-items: center;
 
-    @media (max-width: 767px) {
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
         grid-template-columns: 1fr;
     }
 `;
